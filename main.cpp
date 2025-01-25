f970fda3e33bfd6b93304955d22c3a026db49399
Vicky Kumar Singh <indianarmysniper@gmail.com>
2025-01-25 11:53:29 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "editor.h"
+
+int main() {
+    std::cout << "Welcome to the Native Code Editor!" << std::endl;
+
+    Editor editor;
+    editor.start();
+
+    return 0;
+}
