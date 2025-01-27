bc2be4007f58ff9d9d2a5fa31b8230d44062d9f8
Emma <indianarmysniper@gmail.com>
2025-01-27 22:34:24 +0530

    include files write code for it

---
+++ code_editor/src/inst.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#ifndef UNIX_LINT
+# include <io.h>
+# include <ctype.h>
+
+# include <direct.h>
+
+# include <windows.h>
+# include <pm.h>
+#endif
+
+#ifdef UNIX_LINT
+
+char *searchpath(char *name);
+#endif
+
+#if defined(UNIX_LINT)
+# include <unistd.h>
+# include <errno.h>
+#endif
+
+#include "version.h"
+
+#if defined(UNIX_LINT)
+# define vim_mkdir(x, y) mkdir((char *)(x), y)
+#else
+# define vim_mkdir(x, y) _mkdir((char *)(x))
+#endif
+
+#define sleep(n) Sleep((n) * 1000)
