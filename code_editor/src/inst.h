ba8bd1b86b628d391c4ecfca6d08cf5a11c638d9
Vicky Kumar Singh <indianarmysniper@gmail.com>
2025-01-29 06:06:20 +0530

    Update inst.h

--- code_editor/src/inst.h
+++ code_editor/src/inst.h
@@ -139,4 +139,21 @@ searchpath(char *name)
     static char location[2 * BUFSIZE + 2];
 
 
+    // There appears to be a bug in FindExecutableA() on Windows NT.
+    // Use FindExecutableW() instead...
+    MultiByteToWideChar(CP_ACP, 0, (LPCTSTR)name, -1,
+	    (LPWSTR)widename, BUFSIZE);
+    if (FindExecutableW((LPCWSTR)widename, (LPCWSTR)"",
+		(LPWSTR)location) > (HINSTANCE)32)
+    {
+	WideCharToMultiByte(CP_ACP, 0, (LPWSTR)location, -1,
+		(LPSTR)widename, 2 * BUFSIZE, NULL, NULL);
+	return widename;
+    }
+    return NULL;
+}
+
+
+
+
 
