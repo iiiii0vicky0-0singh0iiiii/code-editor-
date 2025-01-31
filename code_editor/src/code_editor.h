478688d432f03ad0700f261b3f58865626ab99ac
Vicky Kumar Singh <indianarmysniper@gmail.com>
2025-01-31 12:05:46 +0530

    Update code_editor.h

--- code_editor/src/code_editor.h
+++ code_editor/src/code_editor.h
@@ -12,3 +12,27 @@
 #if defined(MSWIN) && !defined(PROTO)
 # include <io.h>
 #endif
+
+
+// ============ the header file puzzle: order matters =========
+
+#ifdef HAVE_CONFIG_H	// GNU autoconf (or something else) was here
+# include "auto/config.h"
+# define HAVE_PATHDEF
+
+/*
+ * Check if configure correctly managed to find sizeof(int).  If this failed,
+ * it becomes zero.  This is likely a problem of not being able to run the
+ * test program.  Other items from configure may also be wrong then!
+ */
+# if (VIM_SIZEOF_INT == 0)
+#  error configure did not run properly.  Check auto/config.log.
+# endif
+
+# if (defined(__linux__) && !defined(__ANDROID__)) || defined(__CYGWIN__) || defined(__GNU__)
+// Needed for strptime().  Needs to be done early, since header files can
+// include other header files and end up including time.h, where these symbols
+// matter for Vim.
+// 700 is needed for mkdtemp().
+#  ifndef _XOPEN_SOURCE
+#   define _XOPEN_SOURCE    700
