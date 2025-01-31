29ccdc6113f6ddca963065c299d5af744bfe1f75
Vicky Kumar Singh <indianarmysniper@gmail.com>
2025-01-31 16:23:06 +0530

    Update screen_display_handle.cpp

--- code_editor/src/screen_display_handle.cpp
+++ code_editor/src/screen_display_handle.cpp
@@ -14,3 +14,31 @@ static int win_do_lines(win_T* wp, int row, int line_count, int mayclear, int de
 static void win_rest_invalid(win_T* wp);
 static void msg_pos_mode();
 static void recording_mode(int attr);
+
+// Ugly global: overrule attribute used by screen_char()
+static int screen_char_attr = 0;
+
+#if defined(FEAT_CONCEAL) || defined(PROTO)
+/*
+* Return TRUE if the cursor line in window "wp" may be concealed, according
+ * to the 'concealcursor' option.
+ */
+    int
+conceal_cursor_line(win_T *wp)
+{
+    int		c;
+
+    if (*wp->w_p_cocu == NUL)
+	return FALSE;
+    if (get_real_state() & MODE_VISUAL)
+	c = 'v';
+    else if (State & MODE_INSERT)
+	c = 'i';
+    else if (State & MODE_NORMAL)
+	c = 'n';
+    else if (State & MODE_CMDLINE)
+	c = 'c';
+    else
+	return FALSE;
+    return vim_strchr(wp->w_p_cocu, c) != NULL;
+}
